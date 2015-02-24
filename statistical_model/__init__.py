d0e2abc07fef01c8da4816dec9a86e6ed3d351c7
David Zwicker <dzwicker@seas.harvard.edu>
2015-02-24 15:42:14 -0500

    Removed temporary project

--- statistical_model/__init__.py
+++
@@ -1,9 +0,0 @@
-from theory import *
-from numerics import *
-from energy_models import *
-
-
-def get_models(energies, num_receptors, num_substrates):
-    model_theory = ReceptorTheory(energies, num_receptors)
-    model_numeric = ReceptorNumerics(energies, num_receptors, num_substrates)
-    return model_theory, model_numeric
